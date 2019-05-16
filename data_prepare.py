c90ff55cc56c2767ca63447903204711382c26a6
Manuel <njester5030@gmail.com>
2019-05-16 18:55:10 +0200

    normalizing method implemented

--- data_prepare.py
+++ data_prepare.py
@@ -1,3 +1,6 @@
+import numpy as np
+from sklearn import preprocessing
+
 class Prepare:
 
 	def prepare(self, data):
@@ -6,6 +9,12 @@ class Prepare:
 		self._one_hot(self, data)
 
 	def _norm(self, data):
+		data[0] = preprocessing.normalize([data[0]])		# age
+		data[5] = preprocessing.normalize([data[5]])		# balance
+		data[11] = preprocessing.normalize([data[11]])	# duration
+		data[12] = preprocessing.normalize([data[12]])	# campaign
+		data[13] = preprocessing.normalize([data[13]])	# pdays
+		data[14] = preprocessing.normalize([data[14]])	# previous
 
 	def _binary(self, data):
 
