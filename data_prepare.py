0b2f201e1c8ee74d557a39716b6ecd688f4c0fb4
Lisa <lisa.zeller@student.fhws.de>
2019-05-16 21:53:15 +0200

    _one_hot function

--- data_prepare.py
+++ data_prepare.py
@@ -1,40 +1,38 @@
 import numpy as np
-from sklearn.preprocessing import minmax_scale
+from sklearn.basepreprocessing import minmax_scale
 from numpy import array
 from numpy import argmax
 from keras.utils import to_categorical
-#from enum_marital
 
 class Prepare:
 
 	def prepare(self, data):
 		self._norm(data)
 		#self._binary(self, data)
-		#self._one_hot()
+		self._one_hot()
 
 	def _norm(self, data):
-		data[0] = minmax_scale(data[0])		#age
+				data[0] = minmax_scale(data[0])		#age
 		data[5] = minmax_scale(data[5])		#balance
 		data[11] = minmax_scale(data[11])	#campaign
 		data[12] = minmax_scale(data[12])	#pdays
 		data[13] = minmax_scale(data[13])	#previous
-		# data[0] = preprocessing.normalize([data[0]])		# age
-		# data[5] = preprocessing.normalize([data[5]])		# balance
-		# data[12] = preprocessing.normalize([data[11]])	# campaign
-		# data[13] = preprocessing.normalize([data[12]])	# pdays
-		# data[14] = preprocessing.normalize([data[13]])	# previous
+		data[0] = preprocessing.normalize([data[0]])		# age
+		data[5] = preprocessing.normalize([data[5]])		# balance
+		data[12] = preprocessing.normalize([data[11]])	# campaign
+		data[13] = preprocessing.normalize([data[12]])	# pdays
+		data[14] = preprocessing.normalize([data[13]])	# previous
 
 	def _binary(self, data):
 		self._binary_para(self,data,6) #default
 		self._binary_para(self,data,8) #housing
 		self._binary_para(self,data,9) #loan
-		#self._binary_para(self,data,15) #deposit
 	def _binary_para(self,data,col):
-		for i in data[col]:	# is yes or no
+		for int i in data[col]:	# is yes or no
 			if data[col][i] == "no" :
-				data[col][i]=0
+				data[col][i]=b
 			else :
-				data[col][i]=1
+				data[col][i]=b
 
 	def _one_hot(self):
 		data = [['married', 'single', 'divorced', 'married', 'married', 'single', 'single', 'single', 'married', 'single'],['secondary', 'secondary','primary', 'secondary','primary', 'tertiary', 'primary', 'unknown']]
@@ -46,92 +44,91 @@ class Prepare:
 		mon = 10
 		pou = 14
 		#job
-		for i in data[job]:
+		for int i in data[job]	for int i in data[job]:
 			if(data[job][i]=="admin."):
-				data[job][i]=0
+				data[job][i]=a
 			
-			elif(data[job][i]== "blue-collar"):
-				data[job][i]=1
+			else if(data[job][i]== "blue-collar"):
+				data[job][i]=b
 			
-			elif(data[job][i]== "entrepreneur"):
-				data[job][i]=2
+			else if(data[job][i]== "entrepreneur"):
+				data[job][i]=c
 			
-			elif(data[job][i]== "housemaid"):
-				data[job][i]=3
+			else if(data[job][i]== "housemaid"):
+				data[job][i]=d
 			
-			elif(data[job][i]== "management"):
-				data[job][i]=4
+			else if(data[job][i]== "management"):
+				data[job][i]=e
 			
-			elif(data[job][i]== "retired"):
-				data[job][i]=5
+			else if(data[job][i]== "retired"):
+				data[job][i]=f
 			
-			elif(data[job][i]== "self-employed"):
-				data[job][i]=6
+			else if(data[job][i]== "self-employed"):
+				data[job][i]=g
 			
-			elif(data[job][i]== "services"):
-				data[job][i]=7
+			else if(data[job][i]== "services"):
+				data[job][i]=h
 			
-			elif(data[job][i]== "student"):
-				data[job][i]=8
+			else if(data[job][i]== "student"):
+				data[job][i]=i
 			
-			elif(data[job][i]== "technician"):
-				data[job][i]=9
+			else if(data[job][i]== "technician"):
+				data[job][i]=j
 			
-			elif(data[job][i]== "services"):
-				data[job][i]=10
+			else if(data[job][i]== "services"):
+				data[job][i]=k
 			
-			elif(data[job][i]== "unemployed"):
-				data[job][i]=11
+			else if(data[job][i]== "unemployed"):
+				data[job][i]=l
 			
 			else :
-				data[job][i]=12
+				data[job][i]=m
 			
-		
 		encodedJob = to_categorical(data[job])
 
 
 
 		#marital
-		for i in data[mari]:
+		for int i in data[mari]	for int i in data[mari]:
 			if(data[mari][i]=="married"):
-				data[mari][i]=0
+				data[mari][i]=a
 			
-			elif(data[mari][i]== "single"):
-				data[mari][i]=1
+			else if(data[mari][i]== "single"):
+				data[mari][i]=b
 			
 			else :
-				data[mari][i]=2
+				data[mari][i]=c
 			
 		
 		encodedMari = to_categorical(data[mari])
 
 		#education
-		for i in data[edu]:
+		for int i in data[edu]:
 			if(data[edu][i]=="primary"):
-				data[edu][i]=0
+				data[edu][i]=a
 			
-			elif(data[edu][i]== "secundary"):
-				data[edu][i]=1
+			else if(data[edu][i]== "secundary"):
+				data[edu][i]=b
 			
-			elif(data[edu][i]== "tertiary"):
-				data[edu][i]=2
+			else if(data[edu][i]== "tertiary"):
+				data[edu][i]=c
 			
 			else :
-				data[edu][i]=3
+				data[edu][i]=d
 			
 		
 		encodedEdu = to_categorical(data[edu])
 		
 		#contact
-		for i in data[con]:
+		for int i in data[con]:
 			if(data[con][i]=="telephone"):
-				data[con][i]=0
+				data[con][i]=a
 			
-			elif(data[con][i]== "cellular"):
-				data[con][i]=1
+			else if(data[con][i]== "cellular"):
+				data[con][i]=b
 			
 			else :
-				data[con][i]=2
+				data[con][i]=c
 			
 		
 		encodedCon = to_categorical(data[con])
@@ -140,79 +137,73 @@ class Prepare:
 		encodedDay = to_categorical(data[day])
 
 		#month
-		for i in data[mon]:
+		for int i in data[mon]	for int i in data[mon]:
 			if(data[mon][i]=="jan"):
-				data[mon][i]=0
+				data[mon][i]=a
 			
-			elif(data[mon][i]== "feb"):
-				data[mon][i]=1
+			else if(data[mon][i]== "feb"):
+				data[mon][i]=b
 			
-			elif(data[mon][i]== "mar"):
-				data[mon][i]=2
+			else if(data[mon][i]== "mar"):
+				data[mon][i]=c
 			
-			elif(data[mon][i]== "apr"):
-				data[mon][i]=3
+			else if(data[mon][i]== "apr"):
+				data[mon][i]=d
 			
-			elif(data[mon][i]== "may"):
-				data[mon][i]=4
+			else if(data[mon][i]== "may"):
+				data[mon][i]=e
 			
-			elif(data[mon][i]== "jun"):
-				data[mon][i]=5
+			else if(data[mon][i]== "jun"):
+				data[mon][i]=f
 			
-			elif(data[mon][i]== "jul"):
-				data[mon][i]=6
+			else if(data[mon][i]== "jul"):
+				data[mon][i]=g
 			
-			elif(data[mon][i]== "aug"):
-				data[mon][i]=7
+			else if(data[mon][i]== "aug"):
+				data[mon][i]=h
 			
-			elif(data[mon][i]== "sep"):
-				data[mon][i]=8
+			else if(data[mon][i]== "sep"):
+				data[mon][i]=i
 			
-			elif(data[mon][i]== "oct"):
-				data[mon][i]=9
+			else if(data[mon][i]== "oct"):
+				data[mon][i]=j
 			
-			elif(data[mon][i]== "nov"):
-				data[mon][i]=10
+			else if(data[mon][i]== "nov"):
+				data[mon][i]=k
 			
 			else :
-				data[mon][i]=12
+				data[mon][i]=l
 			
 		
 		encodedMon = to_categorical(data[mon])
 
 		#poutcome
-		for i in data[pou]:
+		for int i in data[pou]:
 			if(data[pou][i]=="failure"):
-				data[pou][i]=0
+				data[pou][i]=a
 			
-			elif(data[pou][i]== "other"):
-				data[pou][i]=1
+			else if(data[pou][i]== "other"):
+				data[pou][i]=b
 			
-			elif(data[pou][i]== "success"):
-				data[pou][i]=2
+			else if(data[pou][i]== "success"):
+				data[pou][i]=c
 			
 			else :
-				data[pou][i]=3
+				data[pou][i]=d
 			
 		encodedPou = to_categorical(data[pou])
 
-		
 
 		data1 = array(data[0])
 		data2 = array(data[1])
 
-		#transform data in int
-		labelEncoder = preprocessing.LabelEncoder()
-		trafomed_label1 = labelEncoder.fit_transform(data1)
-		trafomed_label2 = labelEncoder.fit_transform(data2)
-		print(trafomed_label1)
-
+		zahlen = 'abcdefghijklmno'
 		# one hot encode
-		encoded1 = to_categorical(trafomed_label1)
-		encoded2 = to_categorical(trafomed_label2)
-		print(encoded1)
-		print(encoded2)
+		onehot_encoded = list()
+		for value in integer_encoded:
+			letter = [0 for _ in range(len(zahlen))]
+			letter[value] = 1
+			onehot_encoded.append(letter)
+		print(onehot_encoded)
 
-		# invert encoding (gibt hinterlegten Wert aus Vektor zurück)
-		#inverted = argmax(encoded[0])
 		
