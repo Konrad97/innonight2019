280820f42453ba750af0d7a1dd391e006d27afe7
Lisa <lisa.zeller@student.fhws.de>
2019-05-16 19:30:01 +0200

    _one_hot Funktion

--- data_prepare.py
+++ data_prepare.py
@@ -5,21 +5,26 @@ from keras.utils import to_categorical
 class Prepare:
 
 	def prepare(self, data):
-		self._norm(self, data)
-		self._binary(self, data)
-		self._one_hot(self, data)
+		#self._norm(self, data)
+		#self._binary(self, data)
+		self._one_hot()
 
-	def _norm(self, data):
+	#def _norm(self, data):
 
-	def _binary(self, data):
+	#def _binary(self, data):
 
-	def _one_hot(self, data):
-		#data = [1, 3, 2, 0, 3, 2, 2, 1, 0, 1]
+	def _one_hot(self):
+		data = [['married', 'single', 'divorced', 'married', 'married', 'single', 'single', 'single', 'married', 'single'],['secondary', 'secondary','primary', 'secondary','primary', 'tertiary', 'primary', 'unknown']]
 		#data = array(data)
 
+		data1 = array(data[0])
+		data2 = array(data[1])
 		# one hot encode
-		encoded = to_categorical(data)
+		encoded1 = to_categorical(data1)
+		encoded2 = to_categorical(data2)
+		print(encoded1)
+		print(encoded2)
 
-		# invert encoding
-		inverted = argmax(encoded[0])
+		# invert encoding (gibt hinterlegten Wert aus Vektor zurück)
+		#inverted = argmax(encoded[0])
 		
