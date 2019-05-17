ecac486ca9850c2948703a9dbe29e3e89454b36e
konrad melzer <konrad.melzer@kurzdigital.com>
2019-05-17 04:08:51 +0200

    lul

--- net.py
+++ net.py
@@ -10,14 +10,13 @@ import datetime
 import os
 import time
 #Read Necessary files
-fullData = pd.read_csv("dataset.csv")
 
 from keras import Sequential
 from keras.layers import Dense, BatchNormalization, Dropout
 from keras.callbacks import TensorBoard, EarlyStopping
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, MaxAbsScaler, RobustScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
@@ -34,31 +33,42 @@ def encode(data, to_encode):
 	del data[to_encode]
 	return data
 
-fullData = encode(fullData, 'job')
-fullData = encode(fullData, 'marital')
-fullData = encode(fullData, 'education')
-fullData = encode(fullData, 'default')
-fullData = encode(fullData, 'housing')
-fullData = encode(fullData, 'loan')
-fullData = encode(fullData, 'contact')
-fullData = encode(fullData, 'day')
-fullData = encode(fullData, 'month')
-#del fullData['day']
-#del fullData['month']
-fullData = encode(fullData, 'poutcome')
-
 def scale(data):
-	mms = MinMaxScaler()
+	mms = RobustScaler()
 	return mms.fit_transform(data)
 
-res = fullData['deposit']
 
-del fullData['deposit']
+def load(file):
 
-fullData = scale(fullData)
+	fullData = pd.read_csv(file)
 
-le = LabelEncoder()
-res = le.fit_transform(res)
+	fullData = encode(fullData, 'job')
+	fullData = encode(fullData, 'marital')
+	fullData = encode(fullData, 'education')
+	fullData = encode(fullData, 'default')
+	fullData = encode(fullData, 'housing')
+	fullData = encode(fullData, 'loan')
+	fullData = encode(fullData, 'contact')
+	fullData = encode(fullData, 'day')
+	fullData = encode(fullData, 'month')
+	#del fullData['day']
+	#del fullData['month']
+	fullData = encode(fullData, 'poutcome')
+
+
+	res = fullData['deposit']
+
+	del fullData['deposit']
+
+	fullData = scale(fullData)
+
+	le = LabelEncoder()
+	res = le.fit_transform(res)
+
+	return [fullData, res]
+
+
+fullData, res = load("dataset.csv")
 
 print (res)
 print(fullData)
@@ -66,14 +76,14 @@ print(fullData)
 train_x, test_x, train_y, test_y = train_test_split(fullData, res, test_size=0.2, random_state=42, shuffle=True)
 
 model = Sequential()
-model.add(Dense(400, input_dim=len(fullData[0]), activation= "relu"))
-model.add(Dropout(0.4))
+model.add(Dense(100, input_dim=len(fullData[0]), activation= "relu"))
+model.add(Dropout(0.2))
 
-model.add(Dense(800, activation= "relu"))
-model.add(Dropout(0.4))
+model.add(Dense(100, activation= "relu"))
+model.add(Dropout(0.2))
 
-model.add(Dense(800, activation= "relu"))
-model.add(Dropout(0.4))
+model.add(Dense(50, activation= "relu"))
+model.add(Dropout(0.2))
 
 model.add(Dense(1, activation= "sigmoid"))
 model.summary()
@@ -86,11 +96,12 @@ time = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H-%M-%S')
 
 tensorboard = TensorBoard(log_dir=os.path.join('tb', time), histogram_freq=0, write_graph=True, write_images=True)
 
-stop = EarlyStopping(monitor='val_loss', min_delta=0, patience=5, verbose=0, mode='auto', baseline=None, restore_best_weights=False)
+stop = EarlyStopping(monitor='val_loss', min_delta=0, patience=5, verbose=0, mode='auto', baseline=None, restore_best_weights=True)
 
 model.fit(train_x, train_y, epochs=200000, validation_data=(test_x, test_y), callbacks=[tensorboard, stop])
 
-
+# Use the load function to load the csv
+# Use the model to run the inference
 
 
 
